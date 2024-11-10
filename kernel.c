151d936511d211ff73c97cc0f56ffe64b9bbb9e0
r1ru <92210252+r1ru@users.noreply.github.com>
2024-11-11 08:55:43 +0900

    Bug fix: Information leak in create_process (#27)
    `create_process`の以下の部分についての修正です。
    ```c
    memcpy((void *) page, image + off, PAGE_SIZE);
    ```
    image_sizeがPAGE_SIZEの倍数でない場合、カーネル内の情報(image +
    image_size以降の情報)がユーザーランドのスタックの後ろにコピーされるため、ユーザーランドからカーネル内の情報を読むことが出来てしまいます。
    
    
    セキュリティを考えるなら修正すべきですが、自作OSでそこまで考えるべきかは微妙なので、実装を簡単に保ちたいという意図であれば無視してください。(とはいえ、簡単には気づかないかつ現実にありそうなバグなので話のネタにはなると思います)
    
    ---------
    
    Co-authored-by: Seiya Nuta <nuta@seiya.me>

--- kernel.c
+++ kernel.c
@@ -438,7 +438,9 @@ struct process *create_process(const void *image, size_t image_size) {
     // User pages.
     for (uint32_t off = 0; off < image_size; off += PAGE_SIZE) {
         paddr_t page = alloc_pages(1);
-        memcpy((void *) page, image + off, PAGE_SIZE);
+        size_t remaining = image_size - off;
+        size_t copy_size = PAGE_SIZE <= remaining ? PAGE_SIZE : remaining;
+        memcpy((void *) page, image + off, copy_size);
         map_page(page_table, USER_BASE + off, page,
                  PAGE_U | PAGE_R | PAGE_W | PAGE_X);
     }
