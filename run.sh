13780346626d494159a01d464f2e90c82453486a
Seiya Nuta <nuta@seiya.me>
2025-01-09 22:01:13 +0900

    Prefer English in the reference implementation

--- run.sh
+++ run.sh
@@ -7,12 +7,12 @@ OBJCOPY=/opt/homebrew/opt/llvm/bin/llvm-objcopy
 
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
-# シェルをビルド
+# Build the shell.
 $CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
 $OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
 $OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
 
-# カーネルをビルド
+# Build the kernel.
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
     kernel.c common.c shell.bin.o
 
