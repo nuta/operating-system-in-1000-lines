322dcd4fb9a125654f5d5d42ffdc5f1627101568
James Young <pronoiac@gmail.com>
2025-01-14 18:01:55 -0600

    Adding items useful for generating an epub with pandoc (#42)
    I based this on how I made an epub for
    [PAIP](https://github.com/norvig/paip-lisp). It's currently a rough
    draft, but it's possibly a useful starting point. This might help with
    #41 .

---
+++ make-epub.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail 
+
+cd website/en
+pandoc \
+  -o ../../"Writing an OS in 1,000 Lines, v0.1-alpha.epub" \
+  ../../epub/title.txt \
+  {index.md,0*.md,1*.md} \
+  --css=../../epub/stylesheet.css \
+  --epub-metadata=../../epub/metadata.xml \
+  --table-of-contents
